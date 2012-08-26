bc7cc7a15981f3bbc1747f80aff0ac9f44f6afc0
Jay Phillips <rocketman8473@gmail.com>
2012-08-26 11:04:54 -0400

    added SerialSevenSeg

---
+++ SixteenSevenSeg/SerialSevenSeg/SerialSevenSeg.ino
@@ -0,0 +1,31 @@
+#include "TM1638.h"// yes you need both
+#include "TM1640.h"
+
+// define a module on data pin 5, clock pin 6
+TM1640 module(5, 6);
+
+char buffer[16];
+byte counter;
+
+void setup()
+{
+  
+  counter = 0;
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  
+  if( Serial.available() )
+  {
+   
+    buffer[counter] = Serial.read();
+    module.setDisplayToString(buffer, 0b0000000000000000);
+
+    counter++; if( counter == 16 ) counter = 0;
+    
+  }
+    
+}
